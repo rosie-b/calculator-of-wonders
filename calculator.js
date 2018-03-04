df3debf70be8723c0f8cf857fc0a792791d67b26
Rosie Burkett <burkettroz@gmail.com>
2018-03-05 10:31:49 +1300

    tidy function name for overwrite check

--- calculator.js
+++ calculator.js
@@ -1,23 +1,23 @@
 var entArr = []; // array of entries to be calculated.
 var entStr = ''; // string of valid entries to be pushed onto the array.
 var result = 0;  // variable to hold the result value, once calculation is complete.
-var check = ''; // string to advise if should overwride the values on the screen
+var overwrite = ''; // string to advise if should overwride the values on the screen
 
-// REFACTOR - function to call elements by id CHECKED AND WORKS (TICK)
+// REFACTORED - function to call elements by id
 function id(x) {
 	return document.getElementById(x);
 }
 
 // add numbers to the entries string and show values on the screen.
 function numBtn(val) {
-		if (check !== 'overwrite') {
+		if (overwrite !== 'Y') {
 			entStr += val
-			id('screen').value += val  // UPDATED
+			id('screen').value += val  
 		} else {
 			entArr = []
 			result = 0
 			entStr = val
-			id('screen').value = val  // UPDATED
+			id('screen').value = val  
 		}	
 	}
 
@@ -26,7 +26,7 @@ function opBtn(val) {
 		entArr.push(entStr)
 		entArr.push(val)
 		entStr = ''
-    id('screen').value += val  // UPDATED
+    id('screen').value += val  
 }
 
 // clear the screen, entArr and entries string, ready for all new values.
@@ -34,12 +34,12 @@ function c() {
 	entArr = []
 	entStr = ''
 	result = 0
-	id('screen').value = ''  // UPDATED
+	id('screen').value = ''  
 }
 
 // function to calculate the entries
 function calc() {
-	id('screen').value = result  // UPDATED
+	id('screen').value = result  
 	entArr.push(entStr)  // .push the lastest entries to the entArr.
 	entStr = '' // empty the entries string, awaiting new values.
 	var x = Number(entArr[0]);  // convert the initial number from string to number.
@@ -58,27 +58,15 @@ function calc() {
 		entArr.push(result)  // result now becomes the new initial number to start subsequent calculations from.
 	}
 
-// event listener to check if screen values and entArr need to be overwritten
+	// event listener to check if screen values and entArr need to be overwritten
 // i.e. last action was evaluate, next button pushed is a number value. So new calculation to commence.
-document.addEventListener('click',overwriteCheck);
-
-/*
-// function to check if should overwrite value and begin new calculation
-function overwriteCheck() {
-	if (entArr.slice(-1)[0] === '+') {check = ''}
-	else if (entArr.slice(-1)[0] === '-') {check = ''}
-	else if (entArr.slice(-1)[0] === '/') {check = ''}
-	else if (entArr.slice(-1)[0] === '*') {check = ''}
-	else if (entArr.slice(-1)[0] === 'undefined') {check = ''}
-	else if (entStr !== '') {check = ''}
-	else {check = 'overwrite'}
-} */
+document.addEventListener('click',check);
 
 // REFACTORED - function to check if should overwrite value and begin new calculation
-function overwriteCheck() {
+function check() {
 	var lastEntry = entArr.slice(-1)[0]
 	var options = ['+', '-', '/', '*', 'undefined']
-	if (options.indexOf(lastEntry) +1) {check = ''}
-	else if (entStr !== '') {check = ''}
-	else {check = 'overwrite'}
+	if (options.indexOf(lastEntry) +1) {overwrite = ''}
+	else if (entStr !== '') {overwrite = ''}
+	else {overwrite = 'Y'}
 }
